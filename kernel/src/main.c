da878eef77551dd04ec2b6324bed8313feb76e54
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2025-12-27 16:47:40 +1100

    update: remove useless includes from main & pmm_init

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,19 +1,19 @@
+#include "common/memory.h"
+
 #include <common/arch.h>
 #include <common/requests.h>
 #include <flanterm.h>
 #include <flanterm_backends/fb.h>
 #include <limine.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
+#include <memory/pmm.h>
 #include <stdio.h>
-#include <string.h>
 
 void kmain(void) {
     verify_requests();
     term_init();
 
     printf("Hello, %s!\n", arch_get_name());
+    pmm_init();
 
     // We're done, just hang...
     arch_die();
