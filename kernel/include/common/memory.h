b4e00c6d2a452d2a6efe3933e3000d2e6f481074
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2025-12-27 16:46:45 +1100

    update: useful memory headers

--- kernel/include/common/memory.h
+++ kernel/include/common/memory.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <arch/memory.h>
 #include <stdint.h>
+
+#define ALIGN_UP(x, align) ((((uintptr_t) (x)) + ((align) - 1)) & ~((uintptr_t) ((align) - 1)))
+#define ALIGN_DOWN(x, align) (((uintptr_t) (x)) & ~((uintptr_t) ((align) - 1)))
 
 typedef uintptr_t phys_addr_t;
 typedef uintptr_t virt_addr_t;
