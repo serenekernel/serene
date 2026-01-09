d38d3768114cd8fd792a73b266bf095f589eae96
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-10 06:58:02 +0800

    minor: change the address range of user processes from starting 16gib to starting at 1gib

--- kernel/src/common/process.c
+++ kernel/src/common/process.c
@@ -12,7 +12,7 @@ extern spinlock_t g_sched_lock;
 process_t* process_create() {
     process_t* process = (process_t*) vmm_alloc_backed(&kernel_allocator, ALIGN_UP(sizeof(process_t), PAGE_SIZE_DEFAULT) / PAGE_SIZE_DEFAULT, VM_ACCESS_KERNEL, VM_CACHE_NORMAL, VM_READ_WRITE, true);
     process->address_space = (vm_allocator_t*) vmm_alloc_backed(&kernel_allocator, 1, VM_ACCESS_KERNEL, VM_CACHE_NORMAL, VM_READ_WRITE, true);
-    vmm_user_init(process->address_space, 0x4000000000, 0x8000000000);
+    vmm_user_init(process->address_space, 0x40000000, 0x80000000);
     process->pid = next_pid++;
 
     uint64_t __spinflags = spinlock_critical_lock(&g_sched_lock);
