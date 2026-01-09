398398d0f6b8ed45805cd062f680e9fa6f045eba
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-09 19:03:56 +0800

    minor: insure that the thread common status is at offset 0x30

--- kernel/include/arch_dep/x86_64/arch/sched.h
+++ kernel/include/arch_dep/x86_64/arch/sched.h
@@ -12,5 +12,9 @@ typedef struct {
     thread_common_t thread_common;
 } __attribute__((packed)) thread_t;
 
+// userspace_stub.asm & sched_stub.asm
 static_assert(offsetof(thread_t, syscall_rsp) == 0x08, "syscall_rsp must be at offset 0x08");
+
+// sched_stub.asm
 static_assert(offsetof(thread_t, kernel_rsp) == 0x10, "kernel_rsp must be at offset 0x10");
+static_assert((offsetof(thread_t, thread_common) + offsetof(thread_common_t, status)) == 0x30, "status must be at offset 0x30");
