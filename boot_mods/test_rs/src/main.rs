732735e63a60c76e3c9ebba88a664d4e545b5064
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-10 06:56:26 +0800

    feat: first ever "program" made for serene! (yes I know it's directly in the kernel main line but oh well)

---
+++ boot_mods/test_rs/src/main.rs
@@ -0,0 +1,89 @@
+#![no_std]
+#![no_main]
+use core::arch::asm;
+use core::fmt;
+use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use x86_64::instructions::port::Port;
+
+#[inline(always)]
+pub fn raw_syscall(a0: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) -> usize {
+    let ret: usize;
+
+    unsafe {
+        asm!(
+            "syscall",
+            in("rdi") a0,
+            in("rsi") a1,
+            in("rdx") a2,
+            in("r10") a3,
+            in("r8")  a4,
+            in("r9")  a5,
+            lateout("rax") ret,
+            lateout("r15") _,
+            lateout("rcx") _,
+            lateout("r11") _,
+            options(nostack),
+        );
+    }
+
+    ret
+}
+
+pub struct Writer;
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        unsafe {
+            Port::new(0xe9).write(byte);
+        }
+    }
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            self.write_byte(byte);
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {});
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => (_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    raw_syscall(0xdeadbeaf, 0xe9, 0, 0, 0, 0);
+    println!("Hello world!");
+    raw_syscall(0xcafebabe, 0, 0, 0, 0, 0);
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
