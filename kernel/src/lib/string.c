1182f982f18834c6e0f358267206643ee7b4abb2
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-13 09:08:57 +1100

    minor: strcmp

--- kernel/src/lib/string.c
+++ kernel/src/lib/string.c
@@ -59,3 +59,21 @@ int memcmp(const void* s1, const void* s2, size_t n) {
 
     return 0;
 }
+
+int strcmp(const char* s1, const char* s2) {
+    while(*s1 != '\0' && *s2 != '\0') {
+        if(*s1 != *s2) {
+            return (*s1 < *s2) ? -1 : 1;
+        }
+        s1++;
+        s2++;
+    }
+
+    if(*s1 == '\0' && *s2 == '\0') {
+        return 0;
+    } else if(*s1 == '\0') {
+        return -1;
+    } else {
+        return 1;
+    }
+}
