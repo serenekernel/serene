02e4aa54dcf75facfa9bdb66fb0b886d808cf40a
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-14 04:29:21 +1100

    minor: strncmp

--- kernel/include/lib/string.h
+++ kernel/include/lib/string.h
@@ -9,3 +9,4 @@ void* memset(void* s, int c, size_t n);
 void* memmove(void* dest, const void* src, size_t n);
 int memcmp(const void* s1, const void* s2, size_t n);
 int strcmp(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, size_t n);
--- kernel/src/lib/string.c
+++ kernel/src/lib/string.c
@@ -77,3 +77,20 @@ int strcmp(const char* s1, const char* s2) {
         return 1;
     }
 }
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+    size_t i = 0;
+
+    while(i < n) {
+        unsigned char c1 = (unsigned char) s1[i];
+        unsigned char c2 = (unsigned char) s2[i];
+
+        if(c1 != c2) return c1 - c2;
+
+        if(c1 == '\0') return 0;
+
+        i++;
+    }
+
+    return 0;
+}
