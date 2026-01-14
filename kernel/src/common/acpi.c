d8d45e4ccc9d0990d9a3580f131d5f39f8a11553
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-14 12:45:27 +1100

    refactor: move from rsdp & xsdp paths to single unified acpi_tables_t

--- kernel/src/common/acpi.c
+++ kernel/src/common/acpi.c
@@ -1,3 +1,5 @@
+#include "common/memory.h"
+#include "memory/vmm.h"
 #include <assert.h>
 #include <common/acpi.h>
 #include <common/requests.h>
@@ -5,16 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ACPI_ENUM_ASDT(asdt, size_of_entry) for(size_t i = 0; i < ((asdt)->header.length - sizeof(acpi_sdt_t)) / (size_of_entry); i++)
-#define ACPI_ASDT                                                          \
-    acpi_xsdt_t* xsdt = ({                                                 \
-        acpi_rsdp_t* rsdp = (acpi_rsdp_t*) rsdp_request.response->address; \
-        if(rsdp->revision >= 2) {                                          \
-            (acpi_xsdt_t*) TO_HHDM(rsdp->xsdt_address);                    \
-        } else {                                                           \
-            (acpi_rsdt_t*) TO_HHDM(rsdp->rsdt_address);                    \
-        }                                                                  \
-    })
+typedef struct {
+    uint8_t acpi_revision;
+    uint8_t number_of_tables;
+    acpi_sdt_t* entries[];
+} acpi_tables_t;
+
+acpi_tables_t* g_tables = nullptr;
 
 bool acpi_checksum(uint8_t* data, size_t length) {
     uint8_t sum = 0;
@@ -30,67 +29,53 @@ void acpi_init(void) {
     assert(strncmp(xsdp->signature, ACPI_SIGNATURE_RSDP, 8) == 0 && "RSDP signature invalid");
     assert(acpi_checksum((uint8_t*) xsdp, sizeof(acpi_rsdp_t)) && "RSDP checksum invalid");
     printf("RSDP revision: %d\n", xsdp->revision);
+
+    uint32_t xsdp_size = (xsdp->revision >= 2) ? sizeof(acpi_xsdp_t) : sizeof(acpi_rsdp_t);
+    uint32_t length = 0;
     if(xsdp->revision >= 2) {
-        assert(acpi_checksum((uint8_t*) xsdp, xsdp->length) && "XSDP checksum invalid");
-
         acpi_xsdt_t* xsdt = (acpi_xsdt_t*) TO_HHDM(xsdp->xsdt_address);
-
-        ACPI_ENUM_ASDT(xsdt, 8) {
-            uint64_t entry_addr = xsdt->entries[i];
-            acpi_sdt_t* entry = (acpi_sdt_t*) TO_HHDM(entry_addr);
-            printf("ACPI Table: %.4s @ 0x%lx\n", entry->signature, (uintptr_t) entry);
-        }
+        length = xsdt->header.length;
+        assert(strncmp(xsdt->header.signature, ACPI_SIGNATURE_XSDT, 4) == 0 && "XSDT signature invalid");
+        assert(acpi_checksum((uint8_t*) xsdt, xsdt->header.length) && "XSDT checksum invalid");
     } else {
         acpi_rsdt_t* rsdt = (acpi_rsdt_t*) TO_HHDM(xsdp->__rsdt_address);
-
-        ACPI_ENUM_ASDT(rsdt, 4) {
-            uint64_t entry_addr = rsdt->entries[i];
-            acpi_sdt_t* entry = (acpi_sdt_t*) TO_HHDM(entry_addr);
-            printf("ACPI Table: %.4s @ 0x%lx\n", entry->signature, (uintptr_t) entry);
-        }
-    }
-}
-
-
-acpi_sdt_t* acpi_find_rsdt(const char* signature) {
-    acpi_rsdp_t* rsdp = (acpi_rsdp_t*) rsdp_request.response->address;
-    acpi_rsdt_t* rsdt = (acpi_rsdt_t*) TO_HHDM(rsdp->rsdt_address);
-
-    ACPI_ENUM_ASDT(rsdt, 4) {
-        uint64_t entry_addr = rsdt->entries[i];
-        acpi_sdt_t* entry = (acpi_sdt_t*) TO_HHDM(entry_addr);
-        if(strncmp(entry->signature, signature, 4) == 0) {
-            return entry;
-        }
+        length = rsdt->header.length;
+        assert(strncmp(rsdt->header.signature, ACPI_SIGNATURE_RSDT, 4) == 0 && "RSDT signature invalid");
+        assert(acpi_checksum((uint8_t*) rsdt, rsdt->header.length) && "RSDT checksum invalid");
     }
 
-    return NULL;
-}
+    uint8_t entry_size = (xsdp->revision >= 2) ? sizeof(uint64_t) : sizeof(uint32_t);
+    uint32_t number_of_tables = (length - sizeof(acpi_sdt_t)) / entry_size;
+    printf("%u %u %u\n", length, xsdp_size, entry_size);
+    printf("ACPI has %u tables\n", number_of_tables);
+    printf("%u\n", sizeof(acpi_tables_t) + (sizeof(uintptr_t) * number_of_tables));
 
-acpi_sdt_t* acpi_find_xsdt(const char* signature) {
-    acpi_xsdp_t* rsdp = (acpi_xsdp_t*) rsdp_request.response->address;
-    acpi_xsdt_t* xsdt = (acpi_xsdt_t*) TO_HHDM(rsdp->xsdt_address);
+    acpi_tables_t* tables = (acpi_tables_t*)vmm_alloc_object(&kernel_allocator, sizeof(acpi_tables_t) + (sizeof(uintptr_t) * number_of_tables));
+    tables->acpi_revision = xsdp->revision;
+    tables->number_of_tables = number_of_tables;
+    g_tables = tables;
 
-    ACPI_ENUM_ASDT(xsdt, 8) {
-        uint64_t entry_addr = xsdt->entries[i];
-        acpi_sdt_t* entry = (acpi_sdt_t*) TO_HHDM(entry_addr);
-        if(strncmp(entry->signature, signature, 4) == 0) {
-            return entry;
-        }
+    for(size_t i = 0; i < number_of_tables; i++) {
+        uintptr_t entry_addr = (xsdp->revision >= 2)
+                                   ? ((acpi_xsdt_t*) TO_HHDM(xsdp->xsdt_address))->entries[i]
+                                   : ((acpi_rsdt_t*) TO_HHDM(xsdp->__rsdt_address))->entries[i];
+        tables->entries[i] = (acpi_sdt_t*) TO_HHDM(entry_addr);
     }
 
-    return NULL;
+    for(size_t i = 0; i < number_of_tables; i++) {
+        acpi_sdt_t* entry = tables->entries[i];
+        printf("ACPI Table: %.4s @ 0x%lx\n", entry->signature, (uintptr_t) entry);
+    }
 }
 
 acpi_sdt_t* acpi_find_table(const char* signature) {
-    acpi_xsdp_t* rsdp = (acpi_xsdp_t*) rsdp_request.response->address;
-    acpi_xsdt_t* xsdt = (acpi_xsdt_t*) TO_HHDM(rsdp->xsdt_address);
-
-    if(rsdp->revision >= 2) {
-        return acpi_find_xsdt(signature);
-    } else {
-        return acpi_find_rsdt(signature);
+    for(size_t i = 0; i < g_tables->number_of_tables; i++) {
+        acpi_sdt_t* entry = g_tables->entries[i];
+        if(strncmp(entry->signature, signature, 4) == 0) {
+            return entry;
+        }
     }
 
+    assert(false && "ACPI table not found");
     return NULL;
 }
