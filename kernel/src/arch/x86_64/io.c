a05f202adb00de5a5a4b1127564fcbdf7842b97c
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-15 07:00:45 +1100

    fix: change io.c from compiler generated memory accesses to inline asm

--- kernel/src/arch/x86_64/io.c
+++ kernel/src/arch/x86_64/io.c
@@ -30,54 +30,102 @@ uint32_t port_read_u32(uint16_t port) {
 
 void mmio_write_u8(virt_addr_t addr, uint8_t value) {
     arch_memory_barrier();
-    *((volatile uint8_t*) addr) = value;
+    
+    __asm__ volatile (
+        "movb %1, (%0)"
+        :
+        : "r"(addr), "q"(value)
+        : "memory"
+    );
+
     arch_memory_barrier();
 }
 
 void mmio_write_u16(virt_addr_t addr, uint16_t value) {
     arch_memory_barrier();
-    *((volatile uint16_t*) addr) = value;
+    
+    __asm__ volatile (
+        "movw %1, (%0)"
+        :
+        : "r"(addr), "q"(value)
+        : "memory"
+    );
+
     arch_memory_barrier();
 }
 
 void mmio_write_u32(virt_addr_t addr, uint32_t value) {
     arch_memory_barrier();
-    *((volatile uint32_t*) addr) = value;
+    __asm__ volatile (
+        "movl %1, (%0)"
+        :
+        : "r"(addr), "q"(value)
+        : "memory"
+    );
     arch_memory_barrier();
 }
 
 void mmio_write_u64(virt_addr_t addr, uint64_t value) {
     arch_memory_barrier();
-    *((volatile uint64_t*) addr) = value;
+    __asm__ volatile (
+        "movq %1, (%0)"
+        :
+        : "r"(addr), "q"(value)
+        : "memory"
+    );
     arch_memory_barrier();
 }
 
 uint8_t mmio_read_u8(virt_addr_t addr) {
     arch_memory_barrier();
-    uint8_t ret = *((volatile uint8_t*) addr);
+    uint8_t ret;
+    __asm__ volatile (
+        "movb (%1), %0"
+        : "=q" (ret)
+        : "r"(addr)
+        : "memory"
+    );
     arch_memory_barrier();
     return ret;
 }
 uint16_t mmio_read_u16(virt_addr_t addr) {
     arch_memory_barrier();
-    uint16_t ret = *((volatile uint16_t*) addr);
+    uint16_t ret;
+    __asm__ volatile (
+        "movw (%1), %0"
+        : "=q" (ret)
+        : "r"(addr)
+        : "memory"
+    );
     arch_memory_barrier();
     return ret;
 }
 uint32_t mmio_read_u32(virt_addr_t addr) {
     arch_memory_barrier();
-    uint32_t ret = *((volatile uint32_t*) addr);
+    uint32_t ret;
+    __asm__ volatile (
+        "movl (%1), %0"
+        : "=q" (ret)
+        : "r"(addr)
+        : "memory"
+    );
     arch_memory_barrier();
     return ret;
 }
 uint64_t mmio_read_u64(virt_addr_t addr) {
     arch_memory_barrier();
-    uint64_t ret = *((volatile uint64_t*) addr);
+    uint64_t ret;
+    __asm__ volatile (
+        "movq (%1), %0"
+        : "=q" (ret)
+        : "r"(addr)
+        : "memory"
+    );
     arch_memory_barrier();
     return ret;
 }
 
-
+// @note: these just live here for now
 [[nodiscard]] uint64_t __rdmsr(uint32_t msr) {
     uint32_t lo, hi;
     __asm__ volatile("rdmsr" : "=a"(lo), "=d"(hi) : "c"(msr));
