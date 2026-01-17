8337425a0d162562efdfddd72a17466b67f14ed3
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-17 12:27:15 +1100

    minor: remove old sched code from aarch64 arch dep

--- kernel/src/arch/aarch64/arch_sched.c
+++ kernel/src/arch/aarch64/arch_sched.c
@@ -1,73 +1,25 @@
-#include <assert.h>
+#include <arch/thread.h>
 #include <memory/memory.h>
-#include <common/process.h>
-#include <common/sched.h>
-#include <common/thread.h>
-#include <common/spinlock.h>
-#include <memory/vmm.h>
 
-// @todo: Implement proper scheduler for aarch64
-// For now, all scheduler functions are stubbed
+#include <assert.h>
 
-typedef struct {
-    thread_t* idle_thread;
-    thread_t* reaper_thread;
-    thread_t* thread_head;
-} scheduler_t;
-
-spinlock_t g_sched_lock = 0;
-scheduler_t g_scheduler = {0};
-
-thread_t* sched_thread_kernel_init(virt_addr_t entry_point) {
-    (void) entry_point;
-    // @todo: 
-    assert(false && "unimplemented");
-    return nullptr;
-}
-
-thread_t* sched_thread_user_init(vm_allocator_t* address_space, virt_addr_t entry_point) {
-    (void) address_space;
-    (void) entry_point;
-    // @todo: 
-    assert(false && "unimplemented");
-    return nullptr;
-}
-
-void sched_add_thread(thread_t* thread) {
-    (void) thread;
-    // @todo: 
+void sched_arch_init_bsp() {
     assert(false && "unimplemented");
 }
 
-void sched_remove_thread(thread_t* thread) {
-    (void) thread;
-    // @todo: 
+void sched_arch_thread_fpu_init(thread_t* thread) {
+    (void)thread;
     assert(false && "unimplemented");
 }
 
-void sched_yield(void) {
-    // @todo: 
+void sched_arch_init_thread(thread_t* thread, virt_addr_t entry_point) {
+    (void)thread;
+    (void)entry_point;
     assert(false && "unimplemented");
 }
 
-void sched_init_bsp(void) {
-    // @todo: 
+void sched_arch_yield_prepare(thread_t* current_thread, thread_t* next_thread) {
+    (void)current_thread;
+    (void)next_thread;
     assert(false && "unimplemented");
-}
-
-void sched_init_ap(void) {
-    // @todo: 
-    assert(false && "unimplemented");
-}
-
-void sched_start_bsp(void) {
-    // @todo: 
-    assert(false && "unimplemented");
-}
-
-thread_t* sched_get_thread(uint32_t tid) {
-    (void) tid;
-    // @todo: 
-    assert(false && "unimplemented");
-    return nullptr;
 }
