dce52ce16bcf0a2ab889d84afa17608d6b5b768e
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-10 00:37:46 +1100

    fix: acpi table parsing

--- kernel/src/common/acpi.c
+++ kernel/src/common/acpi.c
@@ -1,5 +1,6 @@
 #include "memory/memory.h"
 #include "memory/vmm.h"
+
 #include <assert.h>
 #include <common/acpi.h>
 #include <common/requests.h>
@@ -30,6 +31,7 @@ void acpi_init(void) {
     assert(acpi_checksum((uint8_t*) xsdp, sizeof(acpi_rsdp_t)) && "RSDP checksum invalid");
     printf("RSDP revision: %d\n", xsdp->revision);
 
+
     uint32_t xsdp_size = (xsdp->revision >= 2) ? sizeof(acpi_xsdp_t) : sizeof(acpi_rsdp_t);
     uint32_t length = 0;
     if(xsdp->revision >= 2) {
@@ -50,19 +52,21 @@ void acpi_init(void) {
     printf("ACPI has %u tables\n", number_of_tables);
     printf("%u\n", sizeof(acpi_tables_t) + (sizeof(uintptr_t) * number_of_tables));
 
-    acpi_tables_t* tables = (acpi_tables_t*)vmm_alloc_object(&kernel_allocator, sizeof(acpi_tables_t) + (sizeof(uintptr_t) * number_of_tables));
+    if(number_of_tables > 255) {
+        assert(false && "Too many ACPI tables");
+    }
+
+    acpi_tables_t* tables = (acpi_tables_t*) vmm_alloc_object(&kernel_allocator, sizeof(acpi_tables_t) + (sizeof(uintptr_t) * number_of_tables));
     tables->acpi_revision = xsdp->revision;
     tables->number_of_tables = number_of_tables;
     g_tables = tables;
 
     for(size_t i = 0; i < number_of_tables; i++) {
-        uintptr_t entry_addr = (xsdp->revision >= 2)
-                                   ? ((acpi_xsdt_t*) TO_HHDM(xsdp->xsdt_address))->entries[i]
-                                   : ((acpi_rsdt_t*) TO_HHDM(xsdp->__rsdt_address))->entries[i];
+        uintptr_t entry_addr = (xsdp->revision >= 2) ? ((acpi_xsdt_t*) TO_HHDM(xsdp->xsdt_address))->entries[i] : ((acpi_rsdt_t*) TO_HHDM(xsdp->__rsdt_address))->entries[i];
         tables->entries[i] = (acpi_sdt_t*) TO_HHDM(entry_addr);
     }
 
-    for(size_t i = 0; i < number_of_tables; i++) {
+    for(size_t i = 0; i < tables->number_of_tables; i++) {
         acpi_sdt_t* entry = tables->entries[i];
         printf("ACPI Table: %.4s @ 0x%lx\n", entry->signature, (uintptr_t) entry);
     }
