950b94a7aa5c9e42a4d6b72bbdba91c9e090331f
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-13 09:01:53 +1100

    optmize: memcpy and memset

--- kernel/src/lib/string.c
+++ kernel/src/lib/string.c
@@ -17,25 +17,16 @@ size_t strlen(const char* s) {
     return len;
 }
 
-void* memcpy(void* restrict dest, const void* restrict src, size_t n) {
-    uint8_t* restrict pdest = (uint8_t* restrict) dest;
-    const uint8_t* restrict psrc = (const uint8_t* restrict) src;
-
-    for(size_t i = 0; i < n; i++) {
-        pdest[i] = psrc[i];
-    }
-
-    return dest;
+void* memcpy(void* dst, const void* src, size_t n) {
+    void* tmp = dst;
+    asm volatile("rep movsb" : "+D"(dst), "+S"(src), "+c"(n) : : "memory");
+    return tmp;
 }
 
-void* memset(void* s, int c, size_t n) {
-    uint8_t* p = (uint8_t*) s;
-
-    for(size_t i = 0; i < n; i++) {
-        p[i] = (uint8_t) c;
-    }
-
-    return s;
+void* memset(void* dst, int c, size_t n) {
+    void* tmp = dst;
+    asm volatile("rep stosb" : "+D"(dst), "+c"(n) : "a"(c) : "memory");
+    return tmp;
 }
 
 void* memmove(void* dest, const void* src, size_t n) {
